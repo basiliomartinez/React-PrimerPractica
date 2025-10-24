4e19661709e7c4aa9ee4fdd6189c454774596eda
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-10-24 11:05:17 -0300

    Agregamos ejemplo de parrafo Teoria

---
+++ src/components/Teoria.jsx
@@ -0,0 +1,11 @@
+const Teoria = () => {
+    return (
+        <section>
+            <h2 className="alert alert-primary">Que es React?</h2>
+            <p>⚛️ **React** es una biblioteca de JavaScript creada por Meta para construir interfaces de usuario dinámicas. <br />Permite dividir una página en pequeños **componentes reutilizables**, haciendo el código más ordenado, rápido y fácil de mantener.
+</p>
+        </section>
+    );
+};
+
+export default Teoria;
